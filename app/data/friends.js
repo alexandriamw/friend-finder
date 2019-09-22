7a7d3809f7771f343a019c84ce802d7425dcfe68
alexandriamw <alexandriawagner84@gmail.com>
2019-09-22 17:07:20 -0500

    working on js files, added survey questions

---
+++ app/data/friends.js
@@ -0,0 +1,10 @@
+const friendArray = [{
+    "name": "Zandy",
+    "photo": "https://github.com/alexandriamw",
+    "scores": [
+
+    ]
+
+}];
+
+module.exports = friendArray;
--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 // Dependencies
 const express = require("express");
-const path = require("path");
 
 // Sets up the Express App
 const app = express();
@@ -12,7 +11,8 @@ app.use(express.json());
 
 
 
-
+require("./routing/apiRoutes")(app);
+require("./routing/htmlRoutes")(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
