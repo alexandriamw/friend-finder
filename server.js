1753832c7dbd75a3d973b44a544e00c8b5f385ba
alexandriamw <alexandriawagner84@gmail.com>
2019-09-22 15:40:22 -0500

    completed most of home.html and htmlRoutes.js

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ const PORT = process.env.port || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+
+
+
+
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
