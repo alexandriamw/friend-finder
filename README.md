806aa5117819455040fd22080dcb33f1ff14c9c3
alexandriamw <alexandriawagner84@gmail.com>
2019-09-24 14:57:49 -0500

    working on modal, not done yet

---
+++ README.md
@@ -0,0 +1,10 @@
+# friend-finder
+
+Friend-finder is a full-stack compatibility app that takes in the results of a 10 question survey and matches the user with the friend they're most compatible with. 
+
+Check it out on Heroku: 
+
+### NPMs used
+
+- [Express](https://www.npmjs.com/package/express)
+- [Path](https://www.npmjs.com/package/path)
--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,6 @@
 module.exports = [{
-        "name": "Zandy",
-        "photo": "https://avatars2.githubusercontent.com/u/52361438?s=460&v=4",
+        "name": "Giraffe",
+        "photo": "https://animals.sandiegozoo.org/sites/default/files/2016-11/animals_hero_giraffe_1_0.jpg",
         "scores": [
             5,
             4,
@@ -15,8 +15,8 @@ module.exports = [{
         ],
     },
     {
-        "name": "Jeremy",
-        "photo": "https://avatars1.githubusercontent.com/u/1635238?s=460&v=4",
+        "name": "Slimey",
+        "photo": "https://www.farmersalmanac.com/wp-content/uploads/2018/09/Slug-snail-facts-trivia-i956831074.jpeg",
         "scores": [
             1,
             3,
@@ -31,17 +31,65 @@ module.exports = [{
         ]
     },
     {
-        "name": "Darleen",
-        "photo": "https://avatars1.githubusercontent.com/u/17681528?s=460&v=4",
+        "name": "Sloth",
+        "photo": "https://www.nationalgeographic.com/content/dam/animals/2018/10/sloth-energy/01-sloth-energy-nationalgeographic_1046424.jpg",
         "scores": [
             4,
             3,
             2,
-            3,
+            1,
             5,
+            1,
+            2,
+            3,
+            2,
+            4
+        ]
+    },
+    {
+        "name": "Penguin",
+        "photo": "https://thumbs-prod.si-cdn.com/pJr3bbcye9mBiE910wovzd0Yakk=/800x600/filters:no_upscale()/https://public-media.si-cdn.com/filer/96/ea/96ea3332-4453-4baf-9e8a-b187cdbe3eaf/penguin_42-55455098.jpg",
+        "scores": [
+            4,
             5,
             2,
             3,
+            2,
+            5,
+            1,
+            3,
+            2,
+            4
+        ]
+    },
+    {
+        "name": "Puppy",
+        "photo": "https://www.nationalgeographic.com/content/dam/animals/thumbs/rights-exempt/mammals/d/domestic-dog_thumb.jpg",
+        "scores": [
+            5,
+            3,
+            2,
+            1,
+            5,
+            4,
+            2,
+            2,
+            2,
+            4
+        ]
+    },
+    {
+        "name": "Fluffy",
+        "photo": "http://r.ddmcdn.com/s_f/o_1/cx_462/cy_245/cw_1349/ch_1349/w_720/APL/uploads/2015/06/caturday-shutterstock_149320799.jpg",
+        "scores": [
+            3,
+            3,
+            5,
+            3,
+            1,
+            1,
+            2,
+            5,
             2,
             4
         ]
